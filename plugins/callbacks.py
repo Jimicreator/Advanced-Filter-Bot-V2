06e30eb798e0c2a6e08bb1251008fd6e698650f3
Jimicreator <94049086+Jimicreator@users.noreply.github.com>
2021-11-15 20:31:29 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -47,12 +47,12 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("Any Doubt?", url="https://t.me/Jimi_Bots/6"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel 😎 ", url="https://t.me/Jimi_Bots"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/Jimi_Bots/6")
                 ]
             ]
         )
@@ -70,7 +70,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "Share This Bot", url="https://t.me/share/url?url=https://t.me/UltimateFilter_Bot")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
